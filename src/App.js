84fa134d945421b59883f78e7d032c7e7d017915
kitabrhi <youssef.kitabri.123@gmail.com>
2024-07-19 00:05:46 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './Routes';
-import './App.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import ProjectRoutes from './ProjectRoutes';  // Importation de ton fichier avec les routes
+import './App.css';  // Pour les styles
 
 function App() {
     return (
         <Router>
-            <Routes />
+            <div className="app-container">
+                {/* Barre de navigation */}
+                <div className="navigation-buttons">
+                    <button className="nav-btn">
+                        <Link to="/page1">Page 1</Link>
+                    </button>
+
+                    <button className="nav-btn">
+                        <Link to="/page22">Page 2</Link>
+                    </button>
+
+                </div>
+
+                {/* Rendu des routes */}
+                <ProjectRoutes />
+            </div>
         </Router>
     );
 }
